dae36f99882ad799e5f1424e9884c5f4fb0743d2
ravindra rawat <ravinrawat.wd@gmail.com>
2018-07-15 16:31:23 -0600

    signin , register,users,users/:id      -- routrs working

--- server.js
+++ server.js
@@ -5,39 +5,65 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const userData = [
-  {
-    id: "0001",
-    name: "john",
-    email: "john@gmail.com",
-    password: "john123",
-    entries: 0,
-    joined: new Date()
-  },
-  {
-    id: "0002",
-    name: "sam",
-    email: "sam@gmail.com",
-    password: "sam123",
-    entries: 0,
-    joined: new Date()
-  }
-];
+const database = {
+  users: [
+    {
+      id: "0001",
+      name: "john",
+      email: "john@gmail.com",
+      password: "john123",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "0002",
+      name: "sam",
+      email: "sam@gmail.com",
+      password: "sam123",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "0003",
+      name: "molly",
+      email: "molly@gmail.com",
+      password: "molly123",
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
 
 //-----GET "/"
 app.get("/", (req, res) => {
   res.status(200).send("success");
 });
 
+//-----GET "/users"
+app.get("/users", (req, res) => {
+  res.status(200).send(database.users);
+});
+
+//-----GET "/users/:id"
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  database.users.forEach(user => {
+    if (user.id === id) {
+      res.status(200).json(user);
+    }
+  });
+  res.status(404).json("user not found");
+});
+
 //-----POST "/signin"
 app.post("/signin", (req, res) => {
-  console.log(req.body);
-  if (
-    req.body.email === userData[0].email &&
-    req.body.password === userData[0].password
-  ) {
-    res.status(200).json(`welcome ${userData[0].name}`);
-  }
+  database.users.forEach(user => {
+    if (user.email === req.body.email && user.password === req.body.password) {
+      res.status(200).json(`welcome ${user.name}`);
+    }
+  });
+
   res.status(400).json("Incorrect username or password");
 });
 
@@ -45,15 +71,20 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
-  userData.push({
-    id: "0003",
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date()
+  database.users.forEach(user => {
+    if (user.email === req.body.email) {
+      res.status(400).json(`Email already in use`);
+    }
+    database.users.push({
+      id: Math.floor(Math.random() * 10000),
+      name: name,
+      email: email,
+      password: password,
+      entries: 0,
+      joined: new Date()
+    });
+    res.status(200).json(database.users[database.users.length - 1]);
   });
-  res.status(200).json(userData[userData.length - 1]);
 });
 
 app.listen(3005, () => {
